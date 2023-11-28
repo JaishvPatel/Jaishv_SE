a8716efac2b7baa4fe719f098645d44bc26d6be4
JaishvPatel <jaishv181104@gmail.com>
2023-11-28 13:58:17 +0530

    27-11-2023

---
+++ Python/Class/Lecture 1/Q.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter the number : "))
+n2=int(input("Enter the number : "))
+n3=int(input("Enter the number : "))
+n4=int(input("Enter the number : "))
+
+print("The sum of four numbers is : ",n1+n2+n3+n4)
---
+++ Python/Class/Lecture 1/task3.py
@@ -0,0 +1,6 @@
+n=int(input("Enter the number : "))
+
+if(n>0):
+    print(n," is an positive number.")
+else:
+    print(n," is an negative number.")
